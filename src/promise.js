07909fa480c338aa9ce013c9e14c7b1b61893455
张阔 <kuo.zhang@esmart365.com>
2018-07-13 11:31:26 +0800

    11:31

--- src/promise.js
+++ src/promise.js
@@ -1,6 +1,9 @@
 const status = require('status');
 
-const { isThenAble } = require('tool');
+const {
+  isThenAble,
+  changeAble
+} = require('tool');
 
 let Promise = (function(){
 
@@ -26,10 +29,60 @@ let Promise = (function(){
       this.nextPromise.onRejected[0](this.reason);
     }
   }
+  /**
+   * 封装onfulfill和onreject的方法,注册为then生成的promise2的onfulfill和onreject回调
+   * @Params onFulfill  then方法调用时候传入的onFulfill回调，onreject同理
+   * @Params promise2   then方法生成并返回的promise
+   * **/
+
+  /**
+   * 下面根据规范进行onFulfill和onReject的判断
+   * 1.如果 onFulfilled 或者 onRejected 返回一个值 ，promise2变为onfulfilled。
+   * 2.如果 onFulfilled 或者 onRejected 抛出一个异常 e ，则 promise2 必须拒绝执行，并返回拒因 e
+   * 3.如果 onFulfilled 不是函数且 promise1 成功执行， promise2 必须成功执行并返回相同的值
+   * 4.如果 onRejected 不是函数且 promise1 拒绝执行， promise2 必须拒绝执行并返回相同的据因
+   * **/
+  function onFulfillRegister(onFulfill, promise2){
+    return function (value) {
+      /** @Param value 上一个promise成功执行后，传递给下个promise2的value**/
+      //TODO promise 解决过程
+      if(!(onFulfill instanceof Function)){
+        resolve.call(promise2, value);
+        return;
+      }
+      let result;
+      try {
+        result = onFulfill.call(undefined, value);
+      }
+      catch (e) {
+        reject.call(promise2, e);
+      }
+      resolve.call(promise2, result);
+    }
+  }
+  function onRejectRegister(onReject, promise2){
+    return function (reason) {
+      /** @Param reason 上一个promise成功失败后，传递给下个promise2的reason**/
+      if(!(onReject instanceof Function)){
+        reject.call(promise2, reason);
+        return;
+      }
+      let result;
+      try {
+        result = onReject.call(undefined, reason);
+      }
+      catch (e) {
+        reject.call(promise2, e);
+      }
+      resolve.call(promise2, result);
+    }
+  }
 
 
 
-
+  /**
+   * Promise类的主函数
+   * **/
   function Promise(fn) {
     this.status = STATUS[0];
     this.value = undefined;
--- src/tool.js
+++ src/tool.js
@@ -1,3 +1,10 @@
+const status = require('status')
+
+
 exports.isThenAble = function(obj){
   return obj.then && obj.then instanceof Function;
+};
+
+exports.changeAble = function (obj) {
+  return obj.status === status[0];
 };
